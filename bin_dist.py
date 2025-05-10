7ed7dc1aa3e02057fe368401a28ddb7a5209162a
Vishnudrm <vishnurajagopal28@gmail.com>
2025-05-10 21:17:12 +0530

    initial commit

--- bin_dist.py
+++ bin_dist.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import binom
 
-# Parameters
-n = 20  # number of trials
-p = 0.5  # probability of success
+# Get user input
+n = int(input("Enter the number of trials (n): "))
+p = float(input("Enter the probability of success (p between 0 and 1): "))
 
 # Values
 x = np.arange(0, n + 1)
@@ -12,7 +12,7 @@ y = binom.pmf(x, n, p)
 
 # Plot
 plt.plot(x, y, marker='o', linestyle='-', color='blue')
-plt.title('Binomial Distribution (n=20, p=0.5)')
+plt.title(f'Binomial Distribution (n={n}, p={p})')
 plt.xlabel('Number of Successes')
 plt.ylabel('Probability')
 plt.grid(True)
